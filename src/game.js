ca78bfecda5e6ecddf5621a6d2261b926af747cb
DiegoCaballeroRuiz <dicaball@ucm.es>
2024-10-19 17:13:10 +0200

    Phaser añadido
    OOOOOLEEEEE

--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
+let gameGanvas = document.getElementById('juego');
+
 new Phaser.Game({
-  type: Phaser.AUTO,
-  canvas: canvas,
+  type: Phaser.CANVAS,
+  canvas: gameGanvas,
   width: 800,
   height: 400,
   scene: []
