e124838917c1de12bd3ddc0e3a19c77b86f91163
Jordi Llinares <jorllina@ucm.es>
2024-10-22 21:21:34 +0200

    Items

---
+++ src/item.js
@@ -0,0 +1,17 @@
+export default class Item extends Phaser.GameObjects.Sprite {
+    
+
+    itemType = {
+        Piña, Manzanas, Limones, Pepino,
+        Ternera, Pollo, Salchichas,
+        Baguette, Donuts, PanDeMolde,
+        TortillaDePatata, Croquetas, Sushi,
+        Salmon, Gambas, Pulpo,
+        Vino, Vodka,
+        Helado, Pizza,
+        Galletas, Cereales,
+        Leche, Queso,
+        Lejia, Detergente,
+        Condones, Colonia
+    };
+}
