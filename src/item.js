5492f2f86412a6bdb9e3631fba2267a05b871e7d
amielrm4 <amiel42004@gmail.com>
2024-10-24 17:29:39 +0200

    item arreglado (?)
    ya no hago más commits al main

--- src/item.js
+++ src/item.js
@@ -1,14 +1,38 @@
-import ItemType from "./itemType";
 export default class Item extends Phaser.GameObjects.Sprite {
-    /*
-    scene= la escena en la que esta
-    x= posicion x
-    y= posicion y
-    itemType= sting para la imagen (key para el constructor)
-    */ 
+    //itemType= string para la imagen (key para el constructor)
     constructor(scene, x, y, itemType){
+
+        // no es necesario un array de items ?
+        /* let types1 = {
+            piña: 'piña', manzanas: 'manzanas', limones: 'limones', pepino: 'pepino',
+            ternera: 'ternera', pollo: 'pollo', salchichas: 'salchichas',
+            baguette: 'baguette', donuts: 'donuts', panDeMolde: 'panDeMolde',
+            tortillaDePatata: 'tortillaDePatata', croquetas: 'croquetas', sushi: 'sushi',
+            salmon: 'salmon', gambas: 'gambas', pulpo: 'pulpo',
+            vino: 'vino', vodka: 'vodka',
+            helado: 'helado', pizza: 'pizza',
+            galletas: 'galletas', cereales: 'cereales',
+            leche: 'leche', queso: 'queso',
+            lejia: 'lejia', detergente: 'detergente',
+            condones: 'condones', colonia: 'colonia'
+        };
+
+        let types2 = [
+            'piña', 'manzanas', 'limones', 'pepino',
+            'ternera', 'pollo', 'salchichas',
+            'baguette', 'donuts', 'panDeMolde',
+            'tortillaDePatata', 'croquetas', 'sushi',
+            'salmon', 'gambas', 'pulpo',
+            'vino', 'vodka',
+            'helado', 'pizza',
+            'galletas', 'cereales',
+            'leche', 'queso',
+            'lejia', 'detergente',
+            'condones', 'colonia'
+        ]; */
+
         super(scene, x, y, itemType);
         this.scene.add.existing(this);
-        this.typr = itemType;
+        console.log('Item me he creado', itemType);
     }
 }
--- src/mainScene.js
+++ src/mainScene.js
@@ -1,4 +1,5 @@
 import Player from "./player.js";
+import Item from "./item.js";
 
 export default class MainScene extends Phaser.Scene {
     constructor(){
@@ -12,12 +13,20 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         // Cargar assets
         this.load.spritesheet('character', './media/player.png', {frameWidth: 69, frameHeight: 97});
+
+        //en el boot deberiamos hacer un
+        //this.load.spritesheet('id','path a una sola imagen con todos los sprites',{frameWidth: n, frameHeight: m}) //cada nxm es un item
+
+        //PRUEBA ITEM
+        this.load.image('itemPrueba', './media/logoEmpresa.png');
     }
 
     create() {
         // Inicializar objetos
         let player = new Player(this, this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'Jugador');
         //player.moveTo(this.sys.game.config.width / 2, this.sys.game.config.height / 2);
+
+        let itemPrueba = new Item(this, this.sys.game.config.width * 3/4, this.sys.game.config.height * 3/4, 'itemPrueba').setScale(0.025);
 
         //para colisiones
         //this.physics.add.collider(obj1,obj2,()=>{console.log('funcion llamada al colisionar')});
