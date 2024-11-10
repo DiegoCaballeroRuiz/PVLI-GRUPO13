88aee869a40fe3b6e422cbae3f09bbf947206844
Jordi Llinares <jorllina@ucm.es>
2024-11-10 20:58:39 +0100

    pausita y a volver a trabajar

--- src/objetos/stand.js
+++ src/objetos/stand.js
@@ -12,9 +12,10 @@ export default class Stand{
      * @param {Boolean} isVertical Booleano para spawnear los estantes hacia la derecha o hacia abajo
      */
     constructor(scene, x, y, indexArray, isVertical){
+        
         for(let i = 0; i < indexArray.length; ++i){
-            if(isVertical) new Shelf(scene, x, y + i*shelfSize, indexArray[i]);
-            else new Shelf(scene, x + i*shelfSize, y, indexArray[i]);
+            if(isVertical) this.shelfs[i] = Shelf(scene, x, y + i*shelfSize, indexArray[i]);
+            else this.shelfs[i] = Shelf(scene, x + i*shelfSize, y, indexArray[i]);
         }
         this.isVertical = isVertical;
     }
@@ -32,5 +33,6 @@ class Shelf extends Phaser.GameObjects.Sprite {
         super(scene, x, y, 'stand_sprite', 0);
         this.scene.add.existing(this);
         if(itemIndex != -1) this.item = new Item(scene, x, y, itemIndex);
+        return this;
     }
 }
