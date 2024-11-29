007210f085ae35d7f3a4f5ee9c8e2a0c955d2f54
JordiLlinaresSanchez <149331069+JordiLlinaresSanchez@users.noreply.github.com>
2024-11-29 18:57:39 +0100

    las secciones guapas

--- src/escenas/escena.js
+++ src/escenas/escena.js
@@ -1,8 +1,9 @@
 import Player from "../objetos/player.js";
 import Stand from "../objetos/stand.js";
+import Section from "../objetos/sections.js";
 import NPC from "../objetos/npc.js";
 import Carro from "../objetos/carro.js";
-import Section from "../objetos/sections.js";
+import Clock from "../objetos/clock.js";
 import {Shelf as shelf} from "../objetos/stand.js";
 
 export default class Escena extends Phaser.Scene {
@@ -27,14 +28,13 @@ export default class Escena extends Phaser.Scene {
         //this.cameras.main.setBounds(-10, -10, bg.displayWidth+20, bg.displayHeight+20); //crea un cuadrado por donde se puede mover la camara
         //this.cameras.main.startFollow(player);
         
-        let stands = [];
-        stands[0] = new Stand(this, this.sys.game.config.width * 1/4, this.sys.game.config.height * 1/4, [0,1,22,-1,-1,25,8], false);
-        stands[1] = new Stand(this, this.sys.game.config.width * 1/4, this.sys.game.config.height * 1/4 + 64, [0,1,3,-1,-1,9,8], true);
-
         this.player = new Player(this, win_width / 2, win_height / 2, "Jugador");
 
         let sections = []
-        sections[0]= new Section(this, 100, 100, stands, true, 'section1')
+        for (let i=0; i<4; i++){
+            console.log('section'+i);
+            this.sections[i] = new Section(this, 100, 100, [0, 3, 6], 'section'+i);
+        }
 
         this.cameras.main.setBounds(-10, -10, bg.displayWidth+20, bg.displayHeight+20); //crea un cuadrado por donde se puede mover la camara
         this.cameras.main.startFollow(this.player);
