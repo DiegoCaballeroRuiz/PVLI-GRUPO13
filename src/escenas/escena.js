0a8f218a511365151ec2eb70d0377d005fc228f8
Jordi Llinares <jorllina@ucm.es>
2024-12-03 02:43:11 +0100

    ya se ve y se organiza bien

--- src/escenas/escena.js
+++ src/escenas/escena.js
@@ -29,15 +29,23 @@ export default class Escena extends Phaser.Scene {
         //this.cameras.main.startFollow(player);
         
         this.player = new Player(this, win_width / 2, win_height / 2, "Jugador");
-        let sectionIndex = this.procedural(6, 2, 2);
-        this.sections = []
+
+        let gap = 320
+        this.bigSections = []
+        let sectionIndex = this.procedural(4, 1, 2);
+        for (let i = 0; i < sectionIndex[0].length; i++){
+            this.bigSections[i] = new Section(this, gap + 1920 * i, 2*gap, [0, 1, 2, 3], 'bigSection'+ sectionIndex[0][i]);
+        }
+        
+        sectionIndex = this.procedural(6, 2, 2);
+        this.littleSections = []
         for (let i = 0; i < sectionIndex.length; i++){
             for (let j = 0; j < sectionIndex[0].length; j++){
-                this.sections[i] = new Section(this, 320 + 640 * i, 320 + 640 * j, [0, 3, 6], 'section'+ sectionIndex[i][j]);
+                this.littleSections[i * sectionIndex[0].length + j] = new Section(this, 3*gap + 640 * i, gap + 640 * j, [4, 5, 6], 'section'+ sectionIndex[i][j]);
             }
         }
 
-        this.cameras.main.setBounds(-10, -10, bg.displayWidth+20, bg.displayHeight+20); //crea un cuadrado por donde se puede mover la camara
+        this.cameras.main.setBounds(-10, -10, gap*8, gap*4); //crea un cuadrado por donde se puede mover la camara
         this.cameras.main.startFollow(this.player);
 
         
--- src/objetos/sections.js
+++ src/objetos/sections.js
@@ -14,14 +14,24 @@ export default class Section extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, itemIndex, Room){
         super(scene, x, y, Room);
         this.scene.add.existing(this);
-        console.log('Se creo la sala: ' + Room)
-
-        //matriz que incluye los itemIndex y huecos vacios, suffle despues
-        itemIndex = [itemIndex,
-                    [-1, -1, -1]
-        ];
-        shuffle(itemIndex);
-        let standSpawn = [{x:-3,y:-3}, {x:3, y:3}]; //en caso de mas estanterias por seccion, anadir mas posiciones relativas
+        console.log(Room)
+        if(Room === 'bigSection1' || Room === 'bigSection2' || Room === 'bigSection3' || Room === 'bigSection0'){
+            console.log('ha entrado')
+            itemIndex = [[-1, -1, -1],
+                          itemIndex,
+                         [-1, -1, -1]
+                        ];
+            shuffle(itemIndex);
+            var standSpawn = [{x:-3,y:-8}, {x:0, y:0},{x:3, y:8}];
+        }
+        else{
+            //matriz que incluye los itemIndex y huecos vacios, suffle despues
+            itemIndex = [itemIndex,
+                        [-1, -1, -1]
+            ];
+            shuffle(itemIndex);
+            var standSpawn = [{x:-3,y:-3}, {x:3, y:3}]; //en caso de mas estanterias por seccion, anadir mas posiciones relativas
+        }
         this.stands = [];
         let gap = 32;
         for (let i=0; i<itemIndex.length; i++){
@@ -40,8 +50,8 @@ function shuffle(matriz){
     var matrizPlana = matriz.flat(); //devuelve la matriz en un solo array para facilitar el barajado
 
     for(let i = 0; i < matrizPlana.length; i++){
-        //let j = Phaser.Math.RandomDataGenerator.between(i, matrizPlana.lenght);
-        let j = Math.floor(Math.random() * matrizPlana.length - 1);//asi funciona sin phaser
+        let j = Phaser.Math.Between(i, matrizPlana.lenght - 1);
+        //let j = Math.floor(Math.random() * matrizPlana.length - 1);//asi funciona sin phaser
         [matrizPlana[i], matrizPlana[j]] = [matrizPlana[j], matrizPlana[i]]
     }
     for(let i = 0; i < nfilas; ++i){
